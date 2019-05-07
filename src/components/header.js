794293726108c743a62945b9c6ffd99ec0fea148
Lauro Silva <hey@laurosilva.com>
2019-05-07 14:34:37 -0700

    Editing img naming in index and header files

--- src/components/header.js
+++ src/components/header.js
@@ -2,44 +2,46 @@ import React from 'react'
 import Link from 'gatsby-link'
 import './Header.css'
 
-
 class Header extends React.Component {
-    constructor(props) {
-      super(props)
+  constructor(props) {
+    super(props)
 
-      this.state = {
-        hasScrolled: false
-      }
+    this.state = {
+      hasScrolled: false,
     }
+  }
 
-    componentDidMount() {
-      window.addEventListener('scroll',
-      this.handleScroll)
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = event => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50) {
+      this.setState({ hasScrolled: true })
+    } else {
+      this.setState({ hasScrolled: false })
     }
+  }
 
-    handleScroll = (event) => {
-      const scrollTop = window.pageYOffset
-
-      if (scrollTop > 50) {
-        this.setState({ hasScrolled: true })
-      }
-      else {
-        this.setState({ hasScrolled: false})
-      }
-    }
-  
   render() {
     return (
-      <div className={this.state.hasScrolled ?
-      'Header HeaderScrolled' : 'Header'}>
+      <div
+        className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}
+      >
         <div className="HeaderGroup">
-          <Link to="/"><img src={require('../images/please.png')} width="30" /></Link>
+          <Link to="/">
+            <img src={require('../images/please-hero.png')} width="30" />
+          </Link>
           <Link to="/courses">Bananas</Link>
           <Link to="/downloads">Downloads</Link>
           <Link to="/workshops">Workshops</Link>
-          <Link to="/buy"><button>Buy</button></Link>
+          <Link to="/buy">
+            <button>Buy</button>
+          </Link>
         </div>
-      </div> 
+      </div>
     )
   }
 }
