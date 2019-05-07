c08f8856358d572bd0089270a19eeba0a6a7555b
Ryan Hill <ryanhill@Ryans-MBP.home>
2019-05-07 12:27:04 -0400

    Test

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,30 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-
 import Header from '../components/header'
 import './index.css'
+import Footer from '../components/footer';
 
 const Layout = ({ children, data }) => (
   <div>
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content: data.site.siteMetadata.description },
+        { name: 'keywords', content:
+        data.site.siteMetadata.keywords },
       ]}
     />
-    <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <Header />
       {children()}
-    </div>
+      <Footer data={data}>
+      Backgrounds made fresh in Cinema 4d, iOsapp in Swift, site in React <a href="mailto:ryanmack@gmail.com">Email us</a> to ask us anything.
+      </Footer>
   </div>
 )
 
@@ -39,6 +34,16 @@ export const query = graphql`
     site {
       siteMetadata {
         title
+        description
+        keywords
+      }
+    }
+    allContentfulLink (sort: {fields: [createdAt], order: ASC}) {
+      edges {
+        node{
+          title
+          url
+        }
       }
     }
   }
